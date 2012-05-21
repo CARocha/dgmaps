47c50e12ca9dab4da2329e12f7d1f23896d2e4d7
sacrac <crocha09.09@gmail.com>
2012-05-20 18:43:59 -0600

    probando aun no funciona

--- lugares/views.py
+++ lugares/views.py
@@ -5,15 +5,17 @@ from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
 from lugares.models import Lugar
 from forms import LugarForm
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def guardar_mapa(request):
     if request.method == 'POST':
         form = LugarForm(request.POST)
         print form
 
-        if forms.is_valid():
+        if form.is_valid():
             forms_uncommited = form.save(commit=False)
             forms_uncommited.user = request.user
             forms_uncommited.lat = request.POST['lat']
