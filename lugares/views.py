7a8d84038fb9a2d66b0b929c4cba2c07168afb4f
Carlos Rocha <crocha09.09@gmail.com>
2012-05-23 17:33:04 -0600

    fix algunas cosas

--- lugares/views.py
+++ lugares/views.py
@@ -6,6 +6,7 @@ from django.template import RequestContext
 from lugares.models import Lugar
 from forms import LugarForm
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import simplejson
 
 # Create your views here.
 
@@ -17,6 +18,7 @@ def guardar_mapa(request):
         if form.is_valid():
             forms_uncommited = form.save(commit=False)
             forms_uncommited.user = request.user
+            #forms_uncommited.nombre = request.POST['nombre'].escape('%20', ' ')
             forms_uncommited.lat = request.POST['latitud']
             forms_uncommited.lng = request.POST['longitud']
             forms_uncommited.save()
@@ -29,6 +31,17 @@ def guardar_mapa(request):
 
 def mostrar_globo(request):
     globos = Lugar.objects.filter(user=request.user)
+    if request.is_ajax():
+        lista = []
+        for objeto in Lugar.objects.filter(user=request.user):
+            if objeto.lat and objeto.lng:
+                dicc = dict(nombre=objeto.nombre, descripcion=objeto.descripcion,
+                            lon=float(objeto.lng) , lat=float(objeto.lat))
+            lista.append(dicc)
 
-    return render_to_response('lugares/mostrar.html', locals(),
-                                context_instance=RequestContext(request))
+        serializado = simplejson.dumps(lista)
+        print serializado
+        return HttpResponse(serializado, mimetype='application/json')
+
+    #return render_to_response('lugares/mostrar.html', locals(),
+    #                            context_instance=RequestContext(request))
